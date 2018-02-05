fe4d7b0a05cfc3693c8cb918e550f4cbe2bdaf37
christopher.mack <christopher.mack@thomsonreuters.com>
2018-02-05 21:47:17 +0000

    finish existing patient selection and test selection

--- app/src/main/java/com/example/chris/coursework/selection/existing/ExistingPatientView.java
+++ app/src/main/java/com/example/chris/coursework/selection/existing/ExistingPatientView.java
@@ -3,6 +3,7 @@ package com.example.chris.coursework.selection.existing;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TableLayout;
 
 import com.example.chris.coursework.R;
@@ -10,6 +11,10 @@ import com.example.chris.coursework.R;
 public class ExistingPatientView extends AppCompatActivity implements ExistingPatientContract.IExistingPatientView {
 
     private TableLayout tableLayout;
+    private TableLayout sessionsTable;
+
+    private Button newSession;
+    private Button continueSession;
 
     private ExistingPatientPresenter presenter;
 
@@ -21,12 +26,55 @@ public class ExistingPatientView extends AppCompatActivity implements ExistingPa
         presenter = new ExistingPatientPresenter(this);
 
         tableLayout = (TableLayout) findViewById(R.id.tl_existing_patient);
+        sessionsTable = (TableLayout) findViewById(R.id.tl_patient_sessions);
 
+        newSession = (Button) findViewById(R.id.btn_newSession);
+        continueSession = (Button) findViewById(R.id.bt_cont_session);
         presenter.loadPatients();
     }
 
     @Override
     public TableLayout getTableLayout() {
         return this.tableLayout;
+    }
+
+    @Override
+    public void onNewSession(View view) {
+        this.presenter.createNewSession();
+    }
+
+    @Override
+    public void onContinueSession(View view) {
+        this.presenter.continueExistingSession();
+    }
+
+    @Override
+    public TableLayout getSessionsTable() {
+        return sessionsTable;
+    }
+
+    @Override
+    public void setSessionsTable(TableLayout sessionsTable) {
+        this.sessionsTable = sessionsTable;
+    }
+
+    @Override
+    public Button getNewSession() {
+        return newSession;
+    }
+
+    @Override
+    public void setNewSession(Button newSession) {
+        this.newSession = newSession;
+    }
+
+    @Override
+    public Button getContinueSession() {
+        return continueSession;
+    }
+
+    @Override
+    public void setContinueSession(Button continueSession) {
+        this.continueSession = continueSession;
     }
 }
